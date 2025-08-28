4b65b862cb4de76222125eaac278f46310017425
Richa Arora <richaaeri9988@gmail.com>
2025-08-28 07:54:48 +0530

    initial

---
+++ variables.js
@@ -0,0 +1,40 @@
+
+
+
+// {
+// a=9;
+// var b=10;
+// let c=11;
+// c=14;
+// const d=12;
+// console.log(d);
+// }
+
+// const d=20;
+// console.log(d);
+
+// // let and const as block scoped variables
+
+// function show(){
+//     var name="Peter"
+// }
+
+// // var is known  as function scoped variable
+
+
+
+// console.log(num1);
+// let num1=10;
+
+// variable hoisting
+// console.log(num1);
+// var num1=10;
+
+
+// var num1;
+// console.log(num1);
+// num1=10
+
+"use strict"
+a=89;
+console.log(a);
